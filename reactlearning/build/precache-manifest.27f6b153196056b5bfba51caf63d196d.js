20a4f2a19893ff7a2b3c9ddbd8d0d718020e14a7
GeneCPChung <genecpc@gmail.com>
2019-06-24 23:16:19 -0400

    initial commit

---
+++ reactlearning/build/precache-manifest.27f6b153196056b5bfba51caf63d196d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4561ea8546f8f03f9c341fb9b886a83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5acfd800d23e924fe964",
+    "url": "/static/js/2.7fa37ec6.chunk.js"
+  },
+  {
+    "revision": "3fcb120128dadc891c40",
+    "url": "/static/js/main.266d4ae3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
