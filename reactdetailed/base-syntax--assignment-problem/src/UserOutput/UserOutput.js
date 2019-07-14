50a93aeef3e1dd5e9e11151dc92ef76268bd9202
GeneCPChung <genecpc@gmail.com>
2019-07-14 12:02:20 -0400

    updating files

---
+++ reactdetailed/base-syntax--assignment-problem/src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const userOutput = () => {
+    return (
+        <div>
+            <p>Some random text</p>
+            <p>Some random text 2</p>
+        </div>
+    );
+};
+
+export default userOutput;
